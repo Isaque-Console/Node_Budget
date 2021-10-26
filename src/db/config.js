b93284dc1ac7eabfb0ad3e406f7d6589200e7f98
isaque araujo nogueira <isaquean2@gmail.com>
2021-10-25 22:31:57 -0300

    updated db folder

--- src/db/config.js
+++ src/db/config.js
@@ -1,15 +1,13 @@
-// const { Client } = require('pg')
-// const dotenv = require('dotenv')
-// dotenv.config()
+const { Client } = require('pg')
+const dotenv = require('dotenv')
+dotenv.config()
 
-// const client = new Client({
-//     host: process.env.HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.PASSWORD,
-//     port: process.env.PORT,
-//     database: process.env.DATABASE
-// })
+const client = new Client({
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+    database: process.env.DATABASE
+})
 
-// client.connect()
-
-// module.export = client;
+module.exports = client;
--- src/model/Transaction.js
+++ src/model/Transaction.js
@@ -1,16 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
 
-const { Client } = require('pg')
-const dotenv = require('dotenv')
-dotenv.config()
-
-const client = new Client({
-    host: process.env.HOST,
-    user: process.env.DB_USER,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
-    database: process.env.DATABASE
-})
+const client = require("../db/config")
 
 client.connect()
 
